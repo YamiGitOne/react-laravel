05cf2f9f09df242e660aa618cdffab0e435a1838
Yamilet Jerez <quieromiwww@gmail.com>
2023-06-22 21:52:55 +0200

    contextProvider login

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../contexts/ContextProvider"
 
 export default function GuestLayout() {
+    const {token} = useStateContext()
+    
+    //debugger;
+    if (token) {
+        return <Navigate to="/" />
+    }
     return (
         <div>
             para usuarios que no han iniciado sesion
