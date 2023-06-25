daab2246d8e517b7b29402c1899c7fa73313f15e
Yamilet Jerez <quieromiwww@gmail.com>
2023-06-25 16:29:30 +0200

    -

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
@@ -6,11 +6,27 @@ export default function DefaultLayout() {
     if (!token){
         return <Navigate to="/login" />
     }
+    const onLogout = (ev) =>{
+        ev.preventDefault()
+    }
 
     return (
-        <div>
-            para usuarios que si estan logueados
-         <Outlet />
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>header para usuarios que si estan logueados</div>
+                    <div>{user.name}</div>
+                    <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+            
         </div>
     )
 }
