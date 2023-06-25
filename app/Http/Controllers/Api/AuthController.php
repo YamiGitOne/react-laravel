7df78ac4c4b077b24d5e13bb4167bdc6b532dd26
Yamilet Jerez <quieromiwww@gmail.com>
2023-06-25 21:44:56 +0200

    axios login

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //creo tres funciones
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+
+        /**@var /App/Models/User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bccrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response (compact('user', 'token'));
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)){
+            return response([
+                'message' => 'Provided email address or password is incorrect'
+            ]);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response (compact('user', 'token'));
+    }
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccesToken()->delete();
+        return response('', 204);
+    }
+}
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,8 +1,35 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+
 
 export default function Signup() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+    const {} = useStateContext()
+
     const onSubmit = (ev) =>{
         ev.preventDefault()
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        //console.log(payload);
+        axiosClient.post('/signup', payload)
+        .then (({data}) => {
+            setResourceRoot(data.user)
+            setToken(data.token)
+        })
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status === 422) {
+              console.log (response.data.errors);
+            }
+        })
     }
 
     return (
@@ -12,10 +39,10 @@ export default function Signup() {
                 <h1 className="title">
                     Signup for free
                 </h1>
-                <input type="text" placeholder="Full Name" />
-                <input type="email" placeholder="Email Address" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="Password Confirmation" />
+                <input ref={nameRef} placeholder="Full Name" />
+                <input ref={emailRef} type="email" placeholder="Email Address" />
+                <input ref={passwordRef} type="password" placeholder="Password" />
+                <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
                 <button className="btn btn-block">Signup</button>
                 <p className="message">
                     Already Registered? <Link to="/login">Sig in</Link>
